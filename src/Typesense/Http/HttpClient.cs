d6ca719a4da3ba0dec61c920b0f2c5683de6bc20
Mihai <mihai.tirtara@gmail.com>
2020-11-25 15:53:21 +0100

    search paramaters and get request added
    Signed-off-by: Mihai <mihai.tirtara@gmail.com>

--- src/Typesense/Http/HttpClient.cs
+++ src/Typesense/Http/HttpClient.cs
@@ -17,5 +17,16 @@ namespace Typesense.Http
                 return await result.Content.ReadAsStringAsync();
             }
         }
+
+        public async Task<string> Get(string uri, string apiKey)
+        {
+            using (var httpClient = new System.Net.Http.HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("X-TYPESENSE-API-KEY", apiKey);
+
+                var result = await httpClient.GetAsync(uri);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
     }
 }
---
+++ src/Typesense/SearchParameters.cs
@@ -0,0 +1,12 @@
+namespace Typesense
+{
+    public class SearchParameters
+    {
+        public string Q { get; set; }
+        public string QueryBy {get;set;}
+        public string FilterBy {get;set;}
+        public string SortBy {get;set;}
+        public string GroupBy{get;set;}
+        public string GroupLimit{get;set;}
+    }
+}
