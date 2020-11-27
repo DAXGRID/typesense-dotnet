4b965c1e505a8e74f2c23a23b7bd06dc6b7a7d4b
Rune Nielsen <runenielsen@runbox.com>
2020-11-27 10:56:37 +0100

    adds example

---
+++ examples/Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Typesense;
+using Typesense.Setup;
+
+namespace Example
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var provider = new ServiceCollection()
+                .AddTypesenseClient(config =>
+                {
+                    config.ApiKey = "dsfsfs";
+                    config.Nodes = new List<Node> { new Node { Host = "", Port = "2222", Protocol = "http" } };
+                }).BuildServiceProvider();
+
+            var typesenseClient = provider.GetService<ITypesenseClient>();
+            await typesenseClient.CreateCollection(new Schema());
+        }
+    }
+}
