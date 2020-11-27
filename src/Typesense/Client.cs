40efd920218556727ecc7745c6fe066a1c710757
Mihai <mihai.tirtara@gmail.com>
2020-11-27 13:53:42 +0100

    stringbuilder added for search method

--- src/Typesense/Client.cs
+++ src/Typesense/Client.cs
@@ -36,18 +36,35 @@ namespace Typesense
         public async Task Search(string schema, SearchParameters obj)
         {
             var httpClient = new HttpClient();
-            await Get(@$"/collections/{schema}/documents/search?q={obj.Text}
-           &query_by{obj.QueryBy}
-           &filter_by={obj.FilterBy}
-           &sort_by={obj.SortBy}
-           &group_by{obj.GroupBy}
-           &group_limit{obj.GroupLimit}");
+            var builder = new StringBuilder();
+            if(obj.FilterBy != null)
+            {
+                builder.Append($"&filter_by={obj.FilterBy}");
+            }
+            else if(obj.GroupBy!=null)
+            {
+                builder.Append($"&group_by={obj.GroupBy}");
+            }
+            else if(obj.SortBy!=null)
+            {
+                builder.Append($"&sort_by={obj.SortBy}");
+            }
+            else if(obj.GroupLimit!=null)
+            {
+                builder.Append($"&group_limit={obj.GroupLimit}");
+            }
+            else
+            {
+                Console.WriteLine("No any other paramateres added");
+            }
+            Console.WriteLine(builder.ToString());
+            await Get($"/collections/{schema}/documents/search?q={obj.Text}&query_by={obj.QueryBy}"+builder);
         }
 
         public async Task ImportDocuments(string schema, List<object> items, string action)
         {
             var httpClient = new HttpClient();
-            
+
         }
 
         public async Task RetrieveCollection(string schema)
@@ -64,20 +81,26 @@ namespace Typesense
 
         private void ConfigureHttpClient()
         {
-            _httpClient.BaseAddress = new Uri($"{_config.Nodes[0].Host}:{_config.Nodes[0].Port}");
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("X-TYPESENSE-API-KEY", _config.ApiKey);
+            _httpClient.BaseAddress = new Uri($"{_config.Nodes[0].Protocol}://{_config.Nodes[0].Host}:{_config.Nodes[0].Port}");
+            _httpClient.DefaultRequestHeaders.Add("X-TYPESENSE-API-KEY", _config.ApiKey);
         }
 
         private async Task<string> Post(string path, object obj)
         {
-            var jsonString = JsonSerializer.Serialize(obj);
+            var jsonString = obj.ToString();
+            //var jsonString = JsonSerializer.Serialize(obj);
+            Console.WriteLine(jsonString);
             var result = await _httpClient.PostAsync(path, new StringContent(jsonString, Encoding.UTF8, "application/json"));
+            string responseData = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(responseData);
             return await result.Content.ReadAsStringAsync();
         }
 
         private async Task<string> Get(string path)
         {
             var result = await _httpClient.GetAsync(path);
+            string responseData = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(responseData);
             return await result.Content.ReadAsStringAsync();
         }
     }
--- src/Typesense/Field.cs
+++ src/Typesense/Field.cs
@@ -4,6 +4,12 @@ namespace Typesense
 {
     public class Field
     {
+        public Field(string name,string type,bool facet)
+        {
+            Name = name;
+            Type = type;
+            Facet = facet;
+        }
         [JsonPropertyName("name")]
         public string Name { get; set; }
         [JsonPropertyName("type")]
