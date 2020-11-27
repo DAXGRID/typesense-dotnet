4afa09c8581f4dd2f5ad2e5261d7e3b528111f10
Rune Nielsen <runenielsen@runbox.com>
2020-11-27 23:41:09 +0100

    removes dead usings

--- examples/Example/Program.cs
+++ examples/Example/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.Json;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Typesense;
