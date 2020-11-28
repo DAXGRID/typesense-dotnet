e1707ad2f385c60b5a2bf3349781cab1fd65e98c
Rune Nielsen <runenielsen@runbox.com>
2020-11-28 15:14:51 +0100

    removes dead interface member

--- src/Typesense/ITypesenseClient.cs
+++ src/Typesense/ITypesenseClient.cs
@@ -7,7 +7,6 @@ namespace Typesense
         Task CreateCollection(Schema schema);
         Task CreateDocument(string schema, object document);
         Task<SearchResult<T>> Search<T>(string schema, SearchParameters obj);
-        Task RetrieveCollections();
         Task<Collection> RetrieveCollection(string schema);
     }
 }
