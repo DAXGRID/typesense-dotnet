bc5154032d625a998c560879a8efb01a094bf6d8
Rune Nielsen <runenielsen@runbox.com>
2020-12-20 01:36:14 +0100

    adds docstrings for node

--- src/Typesense/Node.cs
+++ src/Typesense/Node.cs
@@ -1,9 +1,21 @@
 namespace Typesense
 {
+    /// <summary>
+    /// The node contains the configuration for a remote Typesense service.
+    /// </summary>
     public class Node
     {
+        /// <summary>
+        /// Hostname for the Typesense service.
+        /// </summary>
         public string Host { get; set; }
+        /// <summary>
+        /// Port for the Typesense service.
+        /// </summary>
         public string Port { get; set; }
-        public string Protocol { get; set; }
+        /// <summary>
+        /// Protocol for the Typesense service - defaults to http.
+        /// </summary>
+        public string Protocol { get; set; } = "http";
     }
 }
