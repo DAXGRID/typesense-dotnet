f66dd661bd523e631515e9a51f0235216d4f4e37
Rune Nielsen <runenielsen@runbox.com>
2021-10-21 17:45:45 +0200

    fix issue matched tokens multiple types (#31)

---
+++ src/Typesense/Converter/MatchedTokenConverter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Typesense.Converter
+{
+    public class MatchedTokenConverter : JsonConverter<IReadOnlyList<object>>
+    {
+        public override IReadOnlyList<object> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var jsonDocument = JsonDocument.ParseValue(ref reader);
+            var matchedTokens = new List<object>();
+
+            foreach (var element in jsonDocument.RootElement.EnumerateArray())
+            {
+                if (element.ValueKind == JsonValueKind.String)
+                {
+                    matchedTokens.Add(element.GetString());
+                }
+                else if (element.ValueKind == JsonValueKind.Array)
+                {
+                    var stringElements = new List<string>();
+                    foreach (var stringElement in element.EnumerateArray())
+                    {
+                        stringElements.Add(stringElement.GetString());
+                    }
+
+                    matchedTokens.Add(stringElements);
+                }
+            }
+
+            return matchedTokens;
+        }
+
+        public override void Write(Utf8JsonWriter writer, IReadOnlyList<object> value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value);
+        }
+    }
+}
