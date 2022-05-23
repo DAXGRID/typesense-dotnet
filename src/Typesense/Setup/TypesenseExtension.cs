5592722dcc9d37c4ce01abf37f24a0bc9d483991
Rune Nielsen <runenielsen@runbox.com>
2022-05-23 16:18:23 +0200

    improves service collection setup

--- src/Typesense/Setup/TypesenseExtension.cs
+++ src/Typesense/Setup/TypesenseExtension.cs
@@ -8,13 +8,12 @@ public static class TypesenseExtension
     /// <exception cref="ArgumentNullException"></exception>
     public static IServiceCollection AddTypesenseClient(this IServiceCollection serviceCollection, Action<Config> config)
     {
-        serviceCollection.AddScoped<ITypesenseClient, TypesenseClient>();
-        serviceCollection.AddHttpClient<ITypesenseClient, TypesenseClient>();
-
         if (config == null)
             throw new ArgumentNullException(nameof(config), $"Please provide options for TypesenseClient.");
 
-        serviceCollection.Configure(config);
-        return serviceCollection;
+        return serviceCollection
+            .AddScoped<ITypesenseClient, TypesenseClient>()
+            .AddHttpClient<ITypesenseClient, TypesenseClient>().Services
+            .Configure(config);
     }
 }
