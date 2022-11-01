7311e2040ee5105b2f2fdb49c29e5d492a3e7dcd
geisterfurz007 <michelvonv@me.com>
2022-11-01 07:38:35 +0100

    fix: group-key typing

--- src/Typesense/SearchResult.cs
+++ src/Typesense/SearchResult.cs
@@ -119,13 +119,13 @@ public record FacetStats
 public record GroupedHit<T>
 {
     [JsonPropertyName("group_key")]
-    public IReadOnlyList<string> GroupKey { get; init; }
+    public IReadOnlyList<object> GroupKey { get; init; }
 
     [JsonPropertyName("hits")]
     public IReadOnlyList<Hit<T>> Hits { get; init; }
 
     [JsonConstructor]
-    public GroupedHit(IReadOnlyList<string> groupKey, IReadOnlyList<Hit<T>> hits)
+    public GroupedHit(IReadOnlyList<object> groupKey, IReadOnlyList<Hit<T>> hits)
     {
         GroupKey = groupKey;
         Hits = hits;
