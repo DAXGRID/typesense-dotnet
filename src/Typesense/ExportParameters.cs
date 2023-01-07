62e83454e82bd8b92f97b2fbcbe7433f94bc6f1e
Fabien Ménager <PhenX@users.noreply.github.com>
2023-01-07 12:46:43 +0100

    Fix user input injections in query parameters (#127)

--- src/Typesense/ExportParameters.cs
+++ src/Typesense/ExportParameters.cs
@@ -1,8 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace Typesense;
 
 public record ExportParameters
 {
+    [JsonPropertyName("filter_by")]
     public string? FilterBy { get; set; }
+    
+    [JsonPropertyName("include_fields")]
     public string? IncludeFields { get; set; }
+    
+    [JsonPropertyName("exclude_fields")]
     public string? ExcludeFields { get; set; }
 }
