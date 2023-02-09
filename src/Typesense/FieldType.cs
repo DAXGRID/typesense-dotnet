898ee73ff277668fc39adb308a736624640760f8
Rune Nielsen <runenielsen@runbox.com>
2023-02-09 19:58:41 +0100

    fields now matches order in docs to easier add new fields

--- src/Typesense/FieldType.cs
+++ src/Typesense/FieldType.cs
@@ -6,30 +6,43 @@ public enum FieldType
 {
     [EnumMember(Value = "string")]
     String,
-    [EnumMember(Value = "int32")]
-    Int32,
-    [EnumMember(Value = "int64")]
-    Int64,
-    [EnumMember(Value = "float")]
-    Float,
-    [EnumMember(Value = "bool")]
-    Bool,
-    [EnumMember(Value = "geopoint")]
-    GeoPoint,
+
     [EnumMember(Value = "string[]")]
     StringArray,
+
+    [EnumMember(Value = "int32")]
+    Int32,
+
     [EnumMember(Value = "int32[]")]
     Int32Array,
+
+    [EnumMember(Value = "int64")]
+    Int64,
+
     [EnumMember(Value = "int64[]")]
     Int64Array,
+
+    [EnumMember(Value = "float")]
+    Float,
+
     [EnumMember(Value = "float[]")]
     FloatArray,
+
+    [EnumMember(Value = "bool")]
+    Bool,
+
     [EnumMember(Value = "bool[]")]
     BoolArray,
+
+    [EnumMember(Value = "geopoint")]
+    GeoPoint,
+
     [EnumMember(Value = "geopoint[]")]
     GeoPointArray,
-    [EnumMember(Value = "auto")]
-    Auto,
+
     [EnumMember(Value = "string*")]
     AutoString,
+
+    [EnumMember(Value = "auto")]
+    Auto,
 }
