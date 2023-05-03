57491bc8e6c4f511b43a2a8abb78d6c2485cfd3d
Rune Nielsen <runenielsen@runbox.com>
2023-05-03 17:03:23 +0200

    can now execute vector queries (#146)

--- src/Typesense/Field.cs
+++ src/Typesense/Field.cs
@@ -31,6 +31,9 @@ public record Field
     [JsonPropertyName("locale")]
     public string? Locale { get; init; }
 
+    [JsonPropertyName("num_dim")]
+    public int? NumberOfDimensions { get; init; }
+
     // This constructor is made to handle inherited classes.
     protected Field(string name)
     {
@@ -41,6 +44,13 @@ public record Field
     {
         Name = name;
         Type = type;
+    }
+
+    public Field(string name, FieldType type, int numberOfDimensions)
+    {
+        Name = name;
+        Type = type;
+        NumberOfDimensions = numberOfDimensions;
     }
 
     public Field(string name, FieldType type, bool? facet = null)
