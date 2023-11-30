d6dfa1e98dfb5f7900c40e72513664a656d5bedb
runeanielsen <runenielsen@runbox.com>
2023-11-30 19:54:17 +0100

    removes dead using statement

--- test/Typesense.Tests/TypesenseClientTests.cs
+++ test/Typesense.Tests/TypesenseClientTests.cs
@@ -3,7 +3,6 @@ using FluentAssertions.Execution;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Xunit;
