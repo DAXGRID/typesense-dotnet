3abe843b96bf9e98332e66de4ff32b4f09562677
Henrik Gedionsen <hg@impact.dk>
2024-04-09 15:04:59 +0200

    Add option to enable HTTP compression to lower response times and reduce traffic.

--- src/Typesense/Setup/TypesenseExtension.cs
+++ src/Typesense/Setup/TypesenseExtension.cs
@@ -1,23 +1,35 @@
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net;
+using System.Net.Http;
 
 namespace Typesense.Setup;
 
 public static class TypesenseExtension
 {
+    /// <param name="enableHttpCompression">
+    /// If set to true, HTTP compression is enabled, lowering response times & reducing traffic for externally hosted Typesense, like Typesense Cloud
+    /// Set to false by default to mimic the old behavior, and not add compression processing overhead on locally hosted Typesense
+    /// </param>
     /// <exception cref="ArgumentNullException"></exception>
-    public static IServiceCollection AddTypesenseClient(this IServiceCollection serviceCollection, Action<Config> config)
+    public static IServiceCollection AddTypesenseClient(this IServiceCollection serviceCollection, Action<Config> config, bool enableHttpCompression = false)
     {
         if (config == null)
             throw new ArgumentNullException(nameof(config), $"Please provide options for TypesenseClient.");
 
-        return serviceCollection
+        var httpClientBuilder = serviceCollection
             .AddScoped<ITypesenseClient, TypesenseClient>()
             .AddHttpClient<ITypesenseClient, TypesenseClient>(client =>
             {
                 client.DefaultRequestVersion = HttpVersion.Version30;
-            }).Services
+            });
+        if (enableHttpCompression)
+            httpClientBuilder = httpClientBuilder
+                .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler
+                {
+                    AutomaticDecompression = DecompressionMethods.All
+                });
+        return httpClientBuilder.Services
             .Configure(config);
     }
 }
