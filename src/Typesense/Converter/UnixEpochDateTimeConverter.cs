f9f642a86060aa42daaff75076dd253ed102d0b1
Oscar Vicente Perez <oscar@solvingproblemswith.technology>
2024-08-22 16:09:09 +0200

    Add CreatedAt

---
+++ src/Typesense/Converter/UnixEpochDateTimeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Typesense.Converter;
+public class UnixEpochDateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.UnixEpoch.AddSeconds(reader.GetInt64());
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(writer);
+
+        writer.WriteStringValue((value - DateTime.UnixEpoch).TotalSeconds.ToString(CultureInfo.InvariantCulture));
+    }
+}
