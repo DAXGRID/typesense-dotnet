7524dc98e325b35dbb27105980e3596fea6c22cd
fendykwan <fendykwandius@gmail.com>
2025-09-01 07:08:04 +0700

    Update TypesenseClient.cs

--- src/Typesense/TypesenseClient.cs
+++ src/Typesense/TypesenseClient.cs
@@ -55,7 +55,6 @@ public class TypesenseClient : ITypesenseClient
 
             _jsonOptionsCamelCaseIgnoreWritingNull = new JsonSerializerOptions(config.Value.JsonSerializerOptions)
             {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
                 DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
             };
         }
@@ -122,7 +121,7 @@ public class TypesenseClient : ITypesenseClient
         var path = upsert
             ? $"/collections/{collection}/documents?action=upsert"
             : $"/collections/{collection}/documents";
-        return Post<T>(path, httpContent, _jsonNameCaseInsensitiveTrue);
+        return Post<T>(path.TrimStart('/'), httpContent, _jsonNameCaseInsensitiveTrue);
     }
 
     private Task<TResult> SearchInternal<TResult>(string collection,
@@ -156,7 +155,7 @@ public class TypesenseClient : ITypesenseClient
             ? "/multi_search"
             : $"/multi_search?limit_multi_searches={limitMultiSearches}";
 
-        var response = await Post<JsonElement>(path, json, jsonSerializerOptions: null, ctk).ConfigureAwait(false);
+        var response = await Post<JsonElement>(path.TrimStart('/'), json, jsonSerializerOptions: null, ctk).ConfigureAwait(false);
 
         return response.TryGetProperty("results", out var results)
             ? results.EnumerateArray().Select(HandleDeserializeMultiSearch<T>).ToList()
@@ -498,7 +497,7 @@ public class TypesenseClient : ITypesenseClient
             path += $"&return_id={returnId}";
         }
 
-        using var response = await _httpClient.PostAsync(path, documents).ConfigureAwait(false);
+        using var response = await _httpClient.PostAsync(path.TrimStart('/'), documents).ConfigureAwait(false);
         if (!response.IsSuccessStatusCode)
             await GetException(response, CancellationToken.None).ConfigureAwait(false);
 
